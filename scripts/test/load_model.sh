0f1c23f28c72aba56a4d519bd638bd5c116d76e9
klhhhhh <1412841649@qq.com>
2025-03-25 20:51:39 -0700

    add test scripts

---
+++ scripts/test/load_model.sh
@@ -0,0 +1,3 @@
+python /global/homes/k/klhhhhh/MetaInit-LLM/nemo_utils/model_loader.py
+    --config-path=/global/homes/k/klhhhhh/MetaInit-LLM/configs \
+    --config-name=megatron_gpt_124m_config \

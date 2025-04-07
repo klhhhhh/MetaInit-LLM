1a6ed5b490c3f7cf49c913d9da6f90806c0d866a
klhhhhh <1412841649@qq.com>
2025-04-06 22:58:14 -0700

    comment eng & add model mapping readme

---
+++ nemo_utils/README.md
@@ -0,0 +1,2 @@
+## Model loader
+1. Need to adjust your weight mapping yaml according to your training script, including model sharding parameters (eg. tensor parallel, pipeline parallel)
--- nemo_utils/model_loader.py
+++ nemo_utils/model_loader.py
@@ -10,12 +10,12 @@ from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import Meg
 from nemo.collections.nlp.parts.megatron_trainer_builder import MegatronTrainerBuilder
 
 def load_model_to_cpu(nemo_path: str):
-    # 获取 ../conf 的绝对路径
+    # Get the absolute path of ../conf
     current_file = Path(__file__).resolve()
     config_dir = (current_file.parent.parent / "conf").as_posix()
     config_path = Path(config_dir) / "megatron_gpt_124m_weight_mapping_config.yaml"
 
-    # 加载配置
+    # Load configuration
     with initialize_config_dir(config_dir=config_dir, version_base="1.1"):
         cfg = compose(config_name="megatron_gpt_124m_weight_mapping_config")
 

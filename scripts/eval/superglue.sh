1364febb04b35570024e2b337feb7840e12bbccb
klhhhhh <1412841649@qq.com>
2025-05-07 12:37:49 -0700

    fix bugs for superglue

---
+++ scripts/eval/superglue.sh
@@ -0,0 +1,13 @@
+export WANDB_API_KEY=54c49dff7abb6ed19894a8aaec8b305d316f0072
+export HF_HOME=/pscratch/sd/k/klhhhhh/hf_cache_lm_eval
+export HF_DATASETS_CACHE=$HF_HOME/datasets
+export TRANSFORMERS_CACHE=$HF_HOME/models
+export HF_DATASETS_TRUST_REMOTE_CODE=1
+
+torchrun --nproc-per-node=1 --no-python lm_eval --model nemo_lm \
+  --model_args path='/pscratch/sd/k/klhhhhh/checkpoints/nemo/exp/unzip',data_parallel_size=1  \
+  --wandb_args project=lm-eval-harness-gpt-super-glue-lm-eval-v1 \
+  --tasks super-glue-lm-eval-v1 \
+  --batch_size 32 \
+  --output_path /pscratch/sd/k/klhhhhh/checkpoints/nemo/exp/super-glue-lm-eval-v1 \
+  --log_samples

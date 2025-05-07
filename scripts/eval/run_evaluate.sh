4bb2e13c34b04115e127ddfd0eec74041f4cedd1
klhhhhh <1412841649@qq.com>
2025-05-06 21:35:03 -0700

    update eval scripts

--- scripts/eval/run_evaluate.sh
+++ scripts/eval/run_evaluate.sh
@@ -1,7 +1,14 @@
 # mkdir -p /work/hdd/bdrw/klin4/checkpoints/nemo/unzipped_checkpoint/gpt124m
 # tar -xvf /work/hdd/bdrw/klin4/checkpoints/nemo/gpt/megatron_gpt.nemo -C /work/hdd/bdrw/klin4/checkpoints/nemo/unzipped_checkpoint/gpt124m
 
-torchrun --nproc-per-node=1 /u/klin4/lm-evaluation-harness/main.py --model nemo_lm \
-  --model_args path='/work/hdd/bdrw/klin4/checkpoints/nemo/unzipped_checkpoint/gpt124m',devices=1 \
-  --tasks lambada_openai,super-glue-lm-eval-v1,winogrande \
-  --batch_size 96
+export WANDB_API_KEY=54c49dff7abb6ed19894a8aaec8b305d316f0072
+export HF_HOME=/pscratch/sd/k/klhhhhh/hf_cache_lm_eval
+export HF_DATASETS_CACHE=$HF_HOME/datasets
+export TRANSFORMERS_CACHE=$HF_HOME/models
+torchrun --nproc-per-node=1 --no-python lm_eval --model nemo_lm \
+  --model_args path='/pscratch/sd/k/klhhhhh/checkpoints/nemo/exp/unzip',data_parallel_size=1  \
+  --wandb_args project=lm-eval-harness-gpt-lambda-openai \
+  --tasks lambada_openai \
+  --batch_size 32 \
+  --output_path /pscratch/sd/k/klhhhhh/checkpoints/nemo/exp/lambada_openai \
+  --log_samples

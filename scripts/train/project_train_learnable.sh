974cac59707d7a43154dc6e9c932b2b0e3b0c8ab
klhhhhh <1412841649@qq.com>
2025-07-27 10:34:35 -0500

    fix bugs in learnable script

--- scripts/train/project_train_learnable.sh
+++ scripts/train/project_train_learnable.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+eval "$(conda shell.bash hook)"
+source /u/klin4/envs/build_nemo.sh
+conda activate nemo
+
+export PATH="/u/klin4/.conda/envs/nemo/bin:$PATH"
+export WANDB_API_KEY=54c49dff7abb6ed19894a8aaec8b305d316f0072
+
+RDZV_FILE="$HOME/.rdzv/rdzv_${SLURM_JOB_ID}.txt"
+mkdir -p "$(dirname $RDZV_FILE)"
+
+# rank 0
+if [ "$SLURM_PROCID" -eq 0 ]; then
+  MASTER_ADDR=$(scontrol show hostnames $SLURM_NODELIST | head -n 1 )
+  MASTER_PORT=$(shuf -i 49152-65535 -n 1)
+  echo "$MASTER_ADDR $MASTER_PORT" > "$RDZV_FILE"
+fi
+
+# all nodes wait
+while [ ! -f "$RDZV_FILE" ]; do sleep 1; done
+
+# all nodes read
+read MASTER_ADDR MASTER_PORT < "$RDZV_FILE"
+export MASTER_ADDR
+export MASTER_PORT
+
+echo "$MASTER_ADDR"
+echo "$MASTER_PORT"
+
 export NVIDIA_PYTORCH_VERSION=24.12
 
 # Project root directory
@@ -20,11 +48,11 @@ NPROC_PER_NODE=1
 SCRIPT_PATH="$PROJECT_DIR/training/projection_init_pretraining.py"
 
 # Parameters passed to Python
-SMALL_MODEL_PATH="/work/hdd/bdrw/klin4/checkpoints/nemo/gpt/megatron_gpt.nemo"
+SMALL_MODEL_PATH="/work/hdd/bdrw/klin4/run_gpt_124m/megatron_gpt/checkpoints_124m/gpt-124-last"
 LARGE_MODEL_CFG_NAME="megatron_gpt_350m_config"
 PROJECT_DEVICE="cpu"
 TRAIN_DEVICE="cuda"
-RANK=64
+RANK=128
 LEARNABLE="--learnable"
 
 # Enable learnable if needed
@@ -34,6 +62,11 @@ LEARNABLE="--learnable"
 torchrun \
   --nnodes=$NNODES \
   --nproc_per_node=$NPROC_PER_NODE \
+  --master_addr $MASTER_ADDR \
+  --master_port $MASTER_PORT \
+  --rdzv_id=gpt_350m_project \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
   "$SCRIPT_PATH" \
   --small_model_path "$SMALL_MODEL_PATH" \
   --large_model_cfg_name "$LARGE_MODEL_CFG_NAME" \
